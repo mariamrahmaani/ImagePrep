a785ff69ff946cd3e57bcf93809382ae3aaaaa32
mariamrahmaani <mariamrahmaani@gmail.com>
2017-08-18 14:20:26 -0700

    Read images from directory

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,1 +1,16 @@
 
+# This code is tested on Jupyter Qt Console
+Import matplotlib.pyplot as plt
+import os
+Import numpy as np
+
+%matplotlib inline
+
+files = [os.path.join('img_align_celeba', file_i) for file_i in os.listdir('img_align_celeba') if file_i.endswith('.jpg')]
+
+ # put all the files in an array of images from img[0] to img[9]
+
+img = [plt.imread(files[i]) for i in range(0,len(files))]
+
+plt.imshow(img[0])
+
